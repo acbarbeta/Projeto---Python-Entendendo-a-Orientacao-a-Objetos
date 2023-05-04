be4a69550bf18d3b6e6786619504373ef32f961a
Ana Clara <aclara.bvicente@gmail.com>
2023-05-04 20:29:04 -0300

    getters e setters seção 05.07

--- conta.py
+++ conta.py
@@ -17,3 +17,15 @@ class Conta:
     def transfere(self, valor, conta_destino):
         self.saca(valor)
         conta_destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+    
+    def get_titular(self):
+        return self.__titular
+    
+    def get_limite(self):
+        return self.__limite
+    
+    def set_limite(self, limite):
+        self.__limite = limite
