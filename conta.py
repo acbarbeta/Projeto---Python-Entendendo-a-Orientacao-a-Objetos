e3727a9495df2623b0d611b24ded6d9e0dfc000d
Ana Clara <aclara.bvicente@gmail.com>
2023-05-04 21:05:28 -0300

    Atividade seção 06

--- conta.py
+++ conta.py
@@ -11,8 +11,15 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
     
+    def __pode_sacar(self, valor_requerido):
+        valor_disponivel_para_saque = self.__saldo + self.__limite
+        return valor_requerido <= valor_disponivel_para_saque
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print("Você não possui limite o suficiente para essa operação")
 
     def transfere(self, valor, conta_destino):
         self.saca(valor)
@@ -33,3 +40,11 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa':'104', 'Bradesco':'237'}
